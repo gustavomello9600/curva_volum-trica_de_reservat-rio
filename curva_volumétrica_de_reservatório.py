41899e1e3eb42139babac08aaca561c65907aeb6
gustavomello9600 <gustavomelloenciv@gmail.com>
2022-05-09 22:08:56 -0300

    code: criação da lógica do simulador

---
+++ curva_volumétrica_de_reservatório.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+IO = None
+
+def gerar_curva_volumétrica() -> IO:
+    escrever_na_planilha(url_da_planilha,
+        simular_reservatório(
+            parâmetros_de_entrada
+        )
+    )
+
+def escrever_na_planilha(url_da_planilha: str, dados: pd.DataFrame) -> IO:
+    pass
+
+gerar_curva_volumétrica()
